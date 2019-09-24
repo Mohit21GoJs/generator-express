eac6ab7a1f1e9b6f4246e5bf0f057cf035a3a900
Mohit Yadav <mohit.developer21@gmail.com>
2019-09-24 14:30:53 +0400

    add changes for npm inquirer for package names

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,82 +1,100 @@
 const Generator = require("yeoman-generator");
 const yosay = require("yosay");
 const chalk = require("chalk");
+const askName = require("inquirer-npm-name");
+const path = require('path');
 
 const pkgManagerMap = {
-  yarn: function() {
+  yarn() {
     this.yarnInstall();
   },
-  npm: function() {
+  npm() {
     this.npmInstall();
   }
 };
 
 module.exports = class extends Generator {
   initializing() {
-    this.log(yosay(chalk.green(`Let's Scaffold This App`)));
+    this.answers = {
+      packageAnswers: {},
+      packageManagerPrefs: {}
+    };
+    this.log(yosay(chalk.green(`Let's Scaffold Awesome Express App`)));
   }
 
   async prompting() {
     const { name } = this.user.git;
-    this.answers = await this.prompt([
+    const { name: appName } = await askName(
       {
-        type: "input",
-        name: "app_name",
-        message: "Your App Name",
-        default: "sample-app"
+        name: "name",
+        message: "Your app name",
+        default: path.basename(process.cwd()),
+        // filter: makeGeneratorName,
+        // validate: str => {
+        //   return str.length > "generator-".length;
+        // }
       },
-      {
-        type: "input",
-        name: "app_version",
-        message: "Your App Version",
-        default: "0.0.1"
-      },
-      {
-        type: "input",
-        name: "app_description",
-        message: "Your App Description",
-        default: "Express Js App"
-      },
-      {
-        type: "input",
-        name: "main_file",
-        message: "Entry Point for the app",
-        default: "index.js"
-      },
-      {
-        type: "input",
-        name: "author",
-        message: "App Author",
-        default: `${name()}`
-      },
-      {
-        type: "input",
-        name: "license",
-        message: "License Type?",
-        default: "ISC"
-      },
-      {
-        type: "list",
-        name: "package_manager",
-        message: "Select a package manager",
-        choices: [
-          { name: "npm", value: "npm" },
-          { name: "yarn", value: "yarn", checked: true },
-          { name: "jspm", value: "jspm", disabled: true }
-        ]
-      }
-    ]);
+      this
+    );
+    this.answers.packageAnswers.app_name = appName;
+    this.answers.packageAnswers = {
+      ...(await this.prompt([
+        {
+          type: "input",
+          name: "app_version",
+          message: "App Version",
+          default: "0.0.1"
+        },
+        {
+          type: "input",
+          name: "app_description",
+          message: "App Description",
+          default: "Express Js App"
+        },
+        {
+          type: "input",
+          name: "main_file",
+          message: "Entry Point for the app",
+          default: "index.js"
+        },
+        {
+          type: "input",
+          name: "author",
+          message: "Author",
+          default: `${name()}`
+        },
+        {
+          type: "input",
+          name: "license",
+          message: "License Type?",
+          default: "ISC"
+        }
+      ])),
+      ...this.answers.packageAnswers
+    };
+
+    this.answers.packageManagerPrefs = await this.prompt({
+      type: "list",
+      name: "package_manager",
+      message: "Select a package manager",
+      choices: [
+        { name: "npm", value: "npm" },
+        { name: "yarn", value: "yarn", checked: true },
+        { name: "jspm", value: "jspm", disabled: true }
+      ]
+    });
   }
+
   writing() {
     this.fs.copyTpl(
       this.templatePath("package.json.tmpl"),
       this.destinationPath("package.json"),
-      { ...this.answers }
+      { ...this.answers.packageAnswers }
     );
   }
 
   install() {
-    const pkgManager = this.answers.package_manager;
+    const pkgManager = this.answers.packageManagerPrefs.package_manager;
     pkgManagerMap[pkgManager] && pkgManagerMap[pkgManager].call(this);
   }
 };
