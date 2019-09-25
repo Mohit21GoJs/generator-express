0cbd00e16cf28ac7068ea9dba24d1b5d235f2081
Mohit Yadav <mohit.developer21@gmail.com>
2019-09-26 02:29:14 +0400

    add package answer interface

--- src/generators/package/index.ts
+++ src/generators/package/index.ts
@@ -1,21 +1,32 @@
-import { BaseGeneratorClass } from '@helpers/baseClass';
+import { BaseGeneratorClass } from "@helpers/baseClass";
+import { appendToObj } from "@app/helpers/util";
 
-
+interface Author {
+  email?: string;
+  username?: string;
+  name?: string
+}
+interface PackageAnswers {
+  version?: string;
+  author?: Author;
+  license?: string;
+  keywords?: Array<string>;
+  indexFile?: string;
+  description?: string;
+}
 export = class extends BaseGeneratorClass {
-  constructor(args, opts){
-    super(args, opts);
+  answers: PackageAnswers = {};
+
+  __askFor(): Promise<PackageAnswers> {
+    return Promise.resolve({});
+  }
+  async prompting() {
+    this.answers = appendToObj(this.answers, await this.__askFor());
   }
 
-  __askFor(){
-    // ask for package json questions
-  }
-  async prompting(){
-    // call prompts
-  }
-  
   writing() {
     const pkg = this._readPkg();
     // update package json and write it
     this._writePkg(pkg);
   }
-}
+};
