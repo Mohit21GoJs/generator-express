b74f97232f11192d97e36e6538415379412b8afa
Mohit Yadav <mohit.developer21@gmail.com>
2019-09-26 18:15:20 +0400

    Add sequential promise method

--- src/generators/app/index.ts
+++ src/generators/app/index.ts
@@ -54,18 +54,7 @@ export default class extends BaseGeneratorClass {
       mkdirp(name, () => {});
       this.destinationRoot(this.destinationPath(name));
     }
-
-    this.composeWith(require.resolve("generator-node/generators/app"), {
-      boilerplate: false,
-      name,
-      projectRoot: "generators",
-      skipInstall: true,
-      travis: false,
-      coveralls: false,
-      editorconfig: false,
-      license: false
-    });
-
+    this.composeWith(require.resolve("../package"), {});
     this.composeWith(require.resolve("../babel"), {});
   }
 
--- src/generators/package/index.ts
+++ src/generators/package/index.ts
@@ -1,10 +1,11 @@
 import { BaseGeneratorClass } from "@helpers/baseClass";
-import { appendToObj } from "@app/helpers/util";
+import get from "lodash/get";
+import { appendToObj, mappedSequentialPromise } from "@app/helpers/util";
 
 interface Author {
   email?: string;
   username?: string;
-  name?: string
+  name?: string;
 }
 interface PackageAnswers {
   version?: string;
@@ -17,11 +18,85 @@ interface PackageAnswers {
 export = class extends BaseGeneratorClass {
   answers: PackageAnswers = {};
 
-  __askFor(): Promise<PackageAnswers> {
-    return Promise.resolve({});
+  async __askFor(): Promise<{}> {
+    const {
+      prompt,
+      user: {
+        git: { email, name }
+      }
+    } = this;
+
+    return mappedSequentialPromise({
+      reducer: (acc, val) => ({ ...acc, ...val.value }),
+      promises: [
+        {
+          value: prompt,
+          thisArg: this,
+          args: {
+            type: "input",
+            name: "version",
+            message: "Package Version",
+            default: "0.0.1"
+          }
+        },
+        {
+          value: prompt,
+          thisArg: this,
+          args: {
+            type: "input",
+            name: "name",
+            message: "Author Name",
+            default: name
+          }
+        },
+        {
+          value: prompt,
+          thisArg: this,
+          args: {
+            type: "input",
+            name: "email",
+            message: "Author Email",
+            default: email
+          }
+        },
+        {
+          value: prompt,
+          thisArg: this,
+          args: {
+            type: "input",
+            name: "description",
+            message: "Module Description",
+            default: "Some description"
+          }
+        },
+        {
+          value: prompt,
+          thisArg: this,
+          args: {
+            type: "input",
+            name: "indexFile",
+            message: "Index File path",
+            default: "index.js"
+          }
+        }
+      ]
+    });
   }
   async prompting() {
-    this.answers = appendToObj(this.answers, await this.__askFor());
+    const promptAnswers = await this.__askFor();
+    const mappedAnswers = {
+      version: get(promptAnswers, "version"),
+      author: {
+        email: get(promptAnswers, "email"),
+        name: get(promptAnswers, "author")
+      },
+      license: get(promptAnswers, "license"),
+      keywords: [],
+      indexFile: get(promptAnswers, "indexFile"),
+      description: get(promptAnswers, "description")
+    };
+    this.answers = appendToObj(this.answers, mappedAnswers);
+    console.log("answers is", this.answers);
   }
 
   writing() {
